f7bfd351893e817e045c9d9cbfc36bd45c6d9ab3
Elmorsy11 <egypt.team.dev>
2024-04-04 11:06:12 +0200

    rrrr

--- pages/_app.js
+++ pages/_app.js
@@ -84,7 +84,7 @@ function MyApp({ Component, pageProps }) {
     <ThemeProvider>
       <SSRProvider>
         <Provider store={store}>
-          Main development
+          Main development prod
           <NextNprogress
             color="#3668E9"
             startPosition={0.3}
