1a90ba9020b23bb5558fb79da07c564daf1151fd
Elmorsy11 <egypt.team.dev>
2024-04-04 11:33:53 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -84,7 +84,7 @@ function MyApp({ Component, pageProps }) {
     <ThemeProvider>
       <SSRProvider>
         <Provider store={store}>
-          stage dev  check
+          stage dev  check 22
           <NextNprogress
             color="#3668E9"
             startPosition={0.3}
